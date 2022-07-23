f014421d8365916e12b0f616c26936bdb3f989c6
kasiviswanath <kasivisu4@gmail.com>
2022-07-23 11:23:28 +0530

    Time: 1067 ms (5.01%), Space: 26 MB (30.56%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Approach 1 : O(n^2)
+    # def containsDuplicate(self, nums: List[int]) -> bool:
+    #     for i in range(len(nums)):
+    #         for j in range(i+1,len(nums)):
+    #             if nums[i]==nums[j]:
+    #                 return True
+    #     return False
+    # Approach 2 : O(n log n)
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(0,len(nums)-1):
+            if(nums[i]==nums[i+1]):
+                return True
+        return False
+        
+    
