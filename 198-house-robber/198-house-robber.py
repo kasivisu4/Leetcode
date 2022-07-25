f494c7dc6af9d5fcc693adbc81f2f9fe260ecd04
kasiviswanath <kasivisu4@gmail.com>
2022-07-25 11:43:14 +0530

    Time: 52 ms (42.02%), Space: 13.9 MB (19.08%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,11 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
+        # Using dp array
+#         dp = [0] * len(nums)
+#         dp[0] = nums[0]
+#         if len(nums)<=1:
+#             return dp[-1]
+#         dp[1] = max(nums[1],nums[0])
+#         for i in range(2,len(nums)):
+#             dp[i]=max(dp[i-2]+nums[i],dp[i-1])
+#         return dp[-1]
+        # Using 2 pointers
+        prev_sum = nums[0]
         if len(nums)<=1:
-            return dp[-1]
-        dp[1] = max(nums[1],nums[0])
+            return prev_sum
+        curr_sum = max(prev_sum,nums[1])
         for i in range(2,len(nums)):
-            dp[i]=max(dp[i-2]+nums[i],dp[i-1])
-        return dp[-1]
-        
+            temp = curr_sum
+            curr_sum = max(prev_sum+nums[i],curr_sum)
+            prev_sum = temp
+        return curr_sum
